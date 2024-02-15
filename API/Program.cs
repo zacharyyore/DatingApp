3ef4b54c8d6b46c9314ec92c7def06230e6e4d8f
zacharyyore <99847397+zacharyyore@users.noreply.github.com>
2024-02-14 23:35:52 -0500

    Added Angular

--- API/Program.cs
+++ API/Program.cs
@@ -11,12 +11,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
