077e039744391634dec0f510543d6682c35f8434
zacharyyore <99847397+zacharyyore@users.noreply.github.com>
2024-02-16 00:04:55 -0500

    Added tokens

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class BaseApiController : ControllerBase{
+        
+    }
+}
